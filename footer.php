ce7f8eb12e51665f63a49f481aa5961cb68bbf7a
Maharshi-Pithava-Ordex <maharshi.pithava.ordex@gmail.com>
2024-03-14 21:48:15 +0530

    update html to php

---
+++ footer.php
@@ -0,0 +1,66 @@
+<?php ?>
+<div class="container-fluid">
+    <div class="row justify-content-center p-2 px-md-5 py-4 bg-white">
+        <div class="col-md-6">
+            <div class="row">
+                <div class="col-md-3 d-flex justify-content-center">
+                    <img src="imgs/sacretarial.png" height="120" alt="">
+                </div>
+                <div class="col-md-9">
+                    <h3 class="redtext fw-bold headingtext">Secretariat</h3>
+                    <h5 class="redtext headingtext">Dr Jigar Shrimali</h5>
+                    <div class="purpletext">
+                        <p class="mb-1">Renus Kidney Hospital</p>
+                        <p class="mb-1">505-502, Golden Icon,</p>
+                        <p class="mb-1">Opposite Medilink Hospital, Near Shivranjani Flyover,</p>
+                        <p class="mb-1">132 Feet Ring Rd, Satellite, Ahmedabad, Gujarat 380015</p>
+                    </div>
+                    <div class="mt-4 d-flex flex-column">
+                        <a href="mailto:dr.jdshrimali@gmail.com" class="links text-decoration-none mb-2"><i
+                                class="fa-regular fa-envelope"></i>
+                            dr.jdshrimali@gmail.com</a>
+                        <a href="tel:9099921224" class="links text-decoration-none mb-2"><i
+                                class="fa-solid fa-phone"></i> +91
+                            9099921224</a>
+                        <!-- <a href="https://rxevents.co.in/" class="links text-decoration-none mb-2"><i
+                                    class="fa-solid fa-globe"></i>
+                                www.rxevents.co.in</a> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 mt-5 mt-md-0">
+            <div class="row">
+                <div class="col-md-3 d-flex justify-content-center">
+                    <img src="https://gujaratheart.com/wp-content/uploads/2023/07/Rx-Events-Logo.png" height="100"
+                        class="mt-2" alt="">
+                </div>
+                <div class="col-md-9">
+                    <h3 class="redtext fw-bold headingtext">Event Organizer</h3>
+                    <div class="purpletext">
+                        <p class="mb-1">Rx Events Pvt. Ltd</p>
+                        <p class="mb-1">401, Shyamak Complex,</p>
+                        <p class="mb-1">Opp. Sahjanand College, Ambawadi,</p>
+                        <p class="mb-1">Ahmedabad - 380015</p>
+                    </div>
+                    <div class="mt-4 d-flex flex-column">
+                        <a href="mailto:contact@rxevents.co.in" class="links text-decoration-none mb-2"><i
+                                class="fa-regular fa-envelope"></i>
+                            contact@rxevents.co.in</a>
+                        <a href="tel:9099331371" class="links text-decoration-none mb-2"><i
+                                class="fa-solid fa-phone"></i> +91 9099331371</a>
+                        <a href="https://rxevents.co.in/" class="links text-decoration-none mb-2"><i
+                                class="fa-solid fa-globe"></i> www.rxevents.co.in</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-12">
+            <p class="mt-4 mb-0 redtext text-center user-select-none">Copyright © 2024 isot2024.com | Website
+                Design & Managed
+                by:
+                [Tech-IT
+                Solutions]</p>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -17,35 +19,9 @@
 
 <body>
     <!-- navbar -->
-    <nav class="navbar navbar-expand-lg bg-white customshadow py-0">
-        <div class="container">
-            <a class="navbar-brand" href="#"><img src="imgs/logo.jpg" class="py-0" height="80" alt=""></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item mx-lg-3">
-                        <a class="nav-link fw-bold" href="index.html">Home</a>
-                    </li>
-                    <li class="nav-item mx-lg-3">
-                        <a class="nav-link fw-bold" href="aboutus.html">About Us</a>
-                    </li>
-                    <li class="nav-item mx-lg-3">
-                        <a class="nav-link fw-bold" href="committee.html">Organizing Committee</a>
-                    </li>
-                    <li class="nav-item mx-lg-3">
-                        <a class="nav-link fw-bold" href="aboutCity.html">Host City</a>
-                    </li>
-                    <li class="nav-item mx-lg-3">
-                        <a class="nav-link fw-bold" href="contactus.html">Contact Us</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php 
+        include 'header.php';
+    ?>
 
     <!-- carousel -->
     <div id="carouselExampleInterval" class="carousel slide carousel-fade" data-bs-ride="carousel">
@@ -359,79 +335,17 @@
     </div>
 
     <!-- footer -->
-    <div class="container-fluid">
-        <div class="row justify-content-center p-2 px-md-5 py-4 bg-white">
-            <div class="col-md-6">
-                <div class="row">
-                    <div class="col-md-3 d-flex justify-content-center">
-                        <img src="imgs/sacretarial.png" height="120" alt="">
-                    </div>
-                    <div class="col-md-9">
-                        <h3 class="redtext fw-bold headingtext">Secretariat</h3>
-                        <h5 class="redtext headingtext">Dr Jigar Shrimali</h5>
-                        <div class="purpletext">
-                            <p class="mb-1">Renus Kidney Hospital</p>
-                            <p class="mb-1">505-502, Golden Icon,</p>
-                            <p class="mb-1">Opposite Medilink Hospital, Near Shivranjani Flyover,</p>
-                            <p class="mb-1">132 Feet Ring Rd, Satellite, Ahmedabad, Gujarat 380015</p>
-                        </div>
-                        <div class="mt-4 d-flex flex-column">
-                            <a href="mailto:dr.jdshrimali@gmail.com" class="links text-decoration-none mb-2"><i
-                                    class="fa-regular fa-envelope"></i>
-                                dr.jdshrimali@gmail.com</a>
-                            <a href="tel:9099921224" class="links text-decoration-none mb-2"><i
-                                    class="fa-solid fa-phone"></i> +91
-                                9099921224</a>
-                            <!-- <a href="https://rxevents.co.in/" class="links text-decoration-none mb-2"><i
-                                    class="fa-solid fa-globe"></i>
-                                www.rxevents.co.in</a> -->
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 mt-5 mt-md-0">
-                <div class="row">
-                    <div class="col-md-3 d-flex justify-content-center">
-                        <img src="https://gujaratheart.com/wp-content/uploads/2023/07/Rx-Events-Logo.png" height="100"
-                            class="mt-2" alt="">
-                    </div>
-                    <div class="col-md-9">
-                        <h3 class="redtext fw-bold headingtext">Event Organizer</h3>
-                        <div class="purpletext">
-                            <p class="mb-1">Rx Events Pvt. Ltd</p>
-                            <p class="mb-1">401, Shyamak Complex,</p>
-                            <p class="mb-1">Opp. Sahjanand College, Ambawadi,</p>
-                            <p class="mb-1">Ahmedabad - 380015</p>
-                        </div>
-                        <div class="mt-4 d-flex flex-column">
-                            <a href="mailto:contact@rxevents.co.in" class="links text-decoration-none mb-2"><i
-                                    class="fa-regular fa-envelope"></i>
-                                contact@rxevents.co.in</a>
-                            <a href="tel:9099331371" class="links text-decoration-none mb-2"><i
-                                    class="fa-solid fa-phone"></i> +91 9099331371</a>
-                            <a href="https://rxevents.co.in/" class="links text-decoration-none mb-2"><i
-                                    class="fa-solid fa-globe"></i> www.rxevents.co.in</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12">
-                <p class="mt-4 mb-0 redtext text-center user-select-none">Copyright © 2024 isot2024.com | Website
-                    Design & Managed
-                    by:
-                    [Tech-IT
-                    Solutions]</p>
-            </div>
-        </div>
-    </div>
+    <?php 
+        include 'footer.php';
+    ?>
 
     <!-- Bootstrap js -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
-        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
-        crossorigin="anonymous"></script>
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
-        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
-        crossorigin="anonymous"></script>
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
