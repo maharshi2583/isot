f5be6ec4936394b8f16e9a3d24e7ec32641f09fd
Maharshi-Pithava-Ordex <maharshi.pithava.ordex@gmail.com>
2024-03-18 16:14:08 +0530

    sponsorship added

---
+++ sponsorship.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ISOT 2024</title>
+    <!-- bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="scss/style.css">
+    <link rel="shortcut icon" href="imgs/logofavicon.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <!-- navbar -->
+    <?php 
+        include 'header.php';
+    ?>
+
+    <div class="container">
+        <div class="row my-5 justify-content-center">
+            <div class="col-12 mb-3">
+                <h1 class="fw-bold text-center redtext headingtext">Sponsorship Opportunities </h1>
+            </div>
+            <div class="col-lg-10 card border-0 customshadow customcard p-3 mb-4">
+                <fieldset class="purplefieldset">
+                    <legend class="fw-bold headingtext">
+                        <h4 class="mb-0">Titanium Sponsorship</h4>
+                    </legend>
+                </fieldset>
+            </div>
+            <div class="col-lg-10 card border-0 customshadow customcard p-3 mb-4">
+                <fieldset class="redfieldset">
+                    <legend class="fw-bold headingtext">
+                        <h4 class="mb-0">Titanium Sponsorship</h4>
+                    </legend>
+                </fieldset>
+            </div>
+        </div>
+    </div>
+
+    <!-- footer -->
+    <?php 
+        include 'footer.php';
+    ?>
+
+    <!-- Bootstrap js -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
+        integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
